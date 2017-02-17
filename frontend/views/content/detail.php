a5f04d5eb66801ffd4f7c808c5644113813f3a56
tuanpv1 <tuantrylook@gmail.com>
2017-02-17 20:25:23 +0700

    detail content

--- frontend/views/content/detail.php
+++ frontend/views/content/detail.php
@@ -6,13 +6,14 @@
  * Time: 8:45 AM
  */
 use common\models\Content;
+use yii\helpers\Url;
 
 ?>
 <div class="columns-container">
     <div class="container" id="columns">
         <!-- breadcrumb -->
         <div class="breadcrumb clearfix">
-            <a class="home" href="#" title="Return to Home">Home</a>
+            <a class="home" href="<?= Url::to(['site/index']) ?>" title="Pando Shop"><?= Yii::t('app','Trang chủ') ?></a>
             <span class="navigation-pipe">&nbsp;</span>
             <a href="#" title="Return to Home">Fashion</a>
             <span class="navigation-pipe">&nbsp;</span>
@@ -62,237 +63,21 @@ use common\models\Content;
                 </div>
                 <!-- ./block category  -->
                 <!-- block best sellers -->
-                <div class="block left-module">
-                    <p class="title_block">BEST SELLERS</p>
-                    <div class="block_content">
-                        <div class="owl-carousel owl-best-sell" data-loop="true" data-nav = "false" data-margin = "0" data-autoplayTimeout="1000" data-autoplay="true" data-autoplayHoverPause = "true" data-items="1">
-                            <ul class="products-block best-sell">
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/product-100x122.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Woman Within Plus Size Flared</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p11.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Woman Within Plus Size Flared</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p12.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Plus Size Rock Star Skirt</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                            </ul>
-                            <ul class="products-block best-sell">
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p13.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Woman Within Plus Size Flared</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p14.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Woman Within Plus Size Flared</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="products-block-left">
-                                        <a href="#">
-                                            <img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p15.jpg" alt="SPECIAL PRODUCTS">
-                                        </a>
-                                    </div>
-                                    <div class="products-block-right">
-                                        <p class="product-name">
-                                            <a href="#">Plus Size Rock Star Skirt</a>
-                                        </p>
-                                        <p class="product-price">$38,95</p>
-                                        <p class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </p>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <?= \frontend\widgets\WidgetNewContent::widget() ?>
                 <!-- ./block best sellers  -->
-
                 <!-- left silide -->
+                <?php if(isset($product_hots)){ ?>
                 <div class="col-left-slide left-module">
                     <ul class="owl-carousel owl-style2" data-loop="true" data-nav = "false" data-margin = "0" data-autoplayTimeout="1000" data-autoplayHoverPause = "true" data-items="1" data-autoplay="true">
-                        <li><a href="#"><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/slide-left.jpg" alt="slide-left"></a></li>
-                        <li><a href="#"><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/slide-left2.jpg" alt="slide-left"></a></li>
-                        <li><a href="#"><img src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/slide-left3.png" alt="slide-left"></a></li>
+                        <?php foreach($product_hots as $item){ /** @var  Content $item */?>
+                        <li><a href="<?= \yii\helpers\Url::to(['content/detail','id'=>$item->id]) ?>"><img style="height: 346px" src="<?= $item->getFirstImageLinkFE() ?>" alt="<?= $item->display_name ?>"></a></li>
+                        <?php  }?>
                     </ul>
                 </div>
+                <?php } ?>
                 <!--./left silde-->
                 <!-- block best sellers -->
-                <div class="block left-module">
-                    <p class="title_block">ON SALE</p>
-                    <div class="block_content product-onsale">
-                        <ul class="product-list owl-carousel" data-loop="true" data-nav = "false" data-margin = "0" data-autoplayTimeout="1000" data-autoplayHoverPause = "true" data-items="1" data-autoplay="true">
-                            <li>
-                                <div class="product-container">
-                                    <div class="left-block">
-                                        <a href="#">
-                                            <img class="img-responsive" alt="product" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/product-260x317.jpg" />
-                                        </a>
-                                        <div class="price-percent-reduction2">-30% OFF</div>
-                                    </div>
-                                    <div class="right-block">
-                                        <h5 class="product-name"><a href="#">Maecenas consequat mauris</a></h5>
-                                        <div class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </div>
-                                        <div class="content_price">
-                                            <span class="price product-price">$38,95</span>
-                                            <span class="price old-price">$52,00</span>
-                                        </div>
-                                    </div>
-                                    <div class="product-bottom">
-                                        <a class="btn-add-cart" title="Add to Cart" href="#add">Add to Cart</a>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="product-container">
-                                    <div class="left-block">
-                                        <a href="#">
-                                            <img class="img-responsive" alt="product" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p35.jpg" />
-                                        </a>
-                                        <div class="price-percent-reduction2">-10% OFF</div>
-                                    </div>
-                                    <div class="right-block">
-                                        <h5 class="product-name"><a href="#">Maecenas consequat mauris</a></h5>
-                                        <div class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </div>
-                                        <div class="content_price">
-                                            <span class="price product-price">$38,95</span>
-                                            <span class="price old-price">$52,00</span>
-                                        </div>
-                                    </div>
-                                    <div class="product-bottom">
-                                        <a class="btn-add-cart" title="Add to Cart" href="#add">Add to Cart</a>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="product-container">
-                                    <div class="left-block">
-                                        <a href="#">
-                                            <img class="img-responsive" alt="product" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/p37.jpg" />
-                                        </a>
-                                        <div class="price-percent-reduction2">-42% OFF</div>
-                                    </div>
-                                    <div class="right-block">
-                                        <h5 class="product-name"><a href="#">Maecenas consequat mauris</a></h5>
-                                        <div class="product-star">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-half-o"></i>
-                                        </div>
-                                        <div class="content_price">
-                                            <span class="price product-price">$38,95</span>
-                                            <span class="price old-price">$52,00</span>
-                                        </div>
-                                    </div>
-                                    <div class="product-bottom">
-                                        <a class="btn-add-cart" title="Add to Cart" href="#add">Add to Cart</a>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <?= \frontend\widgets\WidgetSaleContent::widget() ?>
                 <!-- ./block best sellers  -->
                 <!-- left silide -->
                 <div class="col-left-slide left-module">
